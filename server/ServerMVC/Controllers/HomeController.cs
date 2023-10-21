e7815087c39c90b739dc935cf4714ef2dc562264
kradunches <sasha10131310@gmail.com>
2023-10-21 13:35:17 +0300

    very small fixes

---
+++ server/ServerMVC/Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerMVC.Models;
+using System.Linq;
+
+namespace ServerMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMeasurementRepository measurementRepository;
+        public HomeController(IMeasurementRepository measRepo)
+        {
+            this.measurementRepository = measRepo;
+        }
+
+        public List<object> IncarnateMeasurement(Measurement.TypeOfMeasure typeOfMeasure, DateTime startDate, int daysAmount)
+        {
+            List<object> temperatureChart = new List<object>();
+            List<string> labels = new List<string>();
+            List<decimal> values = new List<decimal>();
+            startDate = startDate.ToUniversalTime();
+            for (int i = 0; i < daysAmount; i++)
+            {
+                Random rnd = new Random(DateTime.Now.Millisecond);
+                IQueryable<IGrouping<int, Measurement>> lookup = measurementRepository.Measurements.Where(p => p.measure_date == startDate.AddDays(i).ToUniversalTime()).GroupBy(p => p.measure_hour);
+                int observations = 6;
+                var tmp = lookup.ToList();
+                while (tmp.Count > observations)
+                {
+                    int rndVal = rnd.Next(0, tmp.Count());
+                    tmp.RemoveAt(rndVal);
+                }
+                foreach (var j in tmp)
+                {
+                    int year = j.Select(p => p.measure_date.Year).First();
+                    int month = j.Select(p => p.measure_date.Month).First();
+                    int day = j.Select(p => p.measure_date.Day).First();
+                    int hour = j.Select(p => p.measure_hour).First();
+                    int minute = j.Select(p => p.measure_min).First();
+                    DateTime outDate = new DateTime(year, month, day, hour, minute, 0);
+                    string outStrDate = outDate.ToString("yyyy-MM-dd HH:mm");
+                    decimal outNumVal = 0;
+                    switch (typeOfMeasure)
+                    {
+                        case Measurement.TypeOfMeasure.t_pov:
+                            outNumVal = j.Select(p => p.t_pov).First();
+                            break;
+                        case Measurement.TypeOfMeasure.far:
+                            outNumVal = j.Select(p => p.far).First();
+                            break;
+                        case Measurement.TypeOfMeasure.rh:
+                            outNumVal = j.Select(p => p.rh).First();
+                            break;
+                        case Measurement.TypeOfMeasure.t:
+                            outNumVal = j.Select(p => p.t).First();
+                            break;
+                        case Measurement.TypeOfMeasure.wind:
+                            outNumVal = j.Select(p => p.wind).First();
+                            break;
+                    }
+                    labels.Add(outStrDate);
+                    values.Add(outNumVal);
+                }
+            }
+            temperatureChart.Add(labels); temperatureChart.Add(values);
+            return temperatureChart;
+        }
+
+        [HttpGet]
+        public List<object> GetTpovData1() => IncarnateMeasurement(Measurement.TypeOfMeasure.t_pov, new DateTime(2023, 09, 05), 1);
+        [HttpGet]
+        public List<object> GetTpovData3() => IncarnateMeasurement(Measurement.TypeOfMeasure.t_pov, new DateTime(2023, 09, 05), 3);
+        [HttpGet]
+        public List<object> GetFarData1() => IncarnateMeasurement(Measurement.TypeOfMeasure.far, new DateTime(2023, 09, 05), 1);
+        [HttpGet]
+        public List<object> GetFarData3() => IncarnateMeasurement(Measurement.TypeOfMeasure.far, new DateTime(2023, 09, 05), 3);
+        [HttpGet]
+        public List<object> GetRhData1() => IncarnateMeasurement(Measurement.TypeOfMeasure.rh, new DateTime(2023, 09, 05), 1);
+        [HttpGet]
+        public List<object> GetRhData3() => IncarnateMeasurement(Measurement.TypeOfMeasure.rh, new DateTime(2023, 09, 05), 3);
+        [HttpGet]
+        public List<object> GetTwindData1() => IncarnateMeasurement(Measurement.TypeOfMeasure.t, new DateTime(2023, 09, 05), 1);
+        [HttpGet]
+        public List<object> GetTwindData3() => IncarnateMeasurement(Measurement.TypeOfMeasure.t, new DateTime(2023, 09, 05), 3);
+        [HttpGet]
+        public List<object> GetWindData1() => IncarnateMeasurement(Measurement.TypeOfMeasure.wind, new DateTime(2023, 09, 05), 1);
+        [HttpGet]
+        public List<object> GetWindData3() => IncarnateMeasurement(Measurement.TypeOfMeasure.wind, new DateTime(2023, 09, 05), 3);
+
+        //[HttpGet]
+        //public IQueryable<Measurement> GetCalendarDate()
+        //{
+        //    var tmp = measurementRepository.Measurements.Where(p => p.measure_date == date.ToUniversalTime());
+        //    return tmp;
+        //}
+        //public IQueryable<Measurement> GetMeasurements(DateTime date = new DateTime())
+        //{
+        //    if (date == new DateTime())
+        //    {
+        //        return measurementRepository.Measurements.Where(p => p.measure_date == new DateTime(2023, 09, 05).ToUniversalTime());
+        //    }
+        //    else
+        //        return measurementRepository.Measurements.Where(p => p.measure_date == date.ToUniversalTime());
+        //}
+        public IQueryable<Measurement> GetMeasurements()
+        {
+            return measurementRepository.Measurements.Where(p => p.measure_date == new DateTime(2023, 09, 05).ToUniversalTime());
+        }
+        //[HttpGet]
+        //public virtual ActionResult GetCalendarDate(DateTime date)
+        //{
+        //    var tmp = measurementRepository.Measurements.Where(p => p.measure_date == date.ToUniversalTime());
+        //    return PartialView(tmp);
+        //}
+        public ViewResult Index() => View(GetMeasurements());
+    }
+}
---
+++ server/ServerMVC/wwwroot/js/fieldchart.js
@@ -0,0 +1,382 @@
+﻿var chartTwind;
+var chartPwind;
+var chartTpov;
+var charWindSpeed;
+
+//TWwind chart
+renderTW = function () {
+    $.ajax({
+        type: "GET",
+        url: "/Home/GetTwindData1",
+        data: '',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: OnSuccessResult,
+        error: OnError
+    });
+    function OnSuccessResult(temperatureChart) {
+        var chartLables = temperatureChart[0];
+        var chartData = temperatureChart[1];
+        var twind = {
+            chart: {
+                height: 280,
+                type: "area"
+            },
+            dataLabels: {
+                enabled: false
+            },
+            series: [
+                {
+                    name: "t воздуха",
+                    data: chartData
+                }
+            ],
+            fill: {
+                type: "gradient",
+                gradient: {
+                    shadeIntensity: 1,
+                    opacityFrom: 0.7,
+                    opacityTo: 0.9,
+                    stops: [0, 90, 100]
+                }
+            },
+            xaxis: {
+                categories: chartLables
+            }
+        };
+        
+        chartTwind = new ApexCharts(document.querySelector(".form__chart--twind"), twind);
+        chartTwind.render();
+    }
+    function OnError(err) {
+
+    }
+}
+
+updateTW = function () {
+    var a = "/Home/GetTwindData1";
+    var tRadio = document.querySelector('input[name=TW]:checked');
+    if (tRadio.value == 1) {
+        a = "/Home/GetTwindData1";
+    }
+    else if (tRadio.value == 3) {
+        a = "/Home/GetTwindData3";
+    }
+    $.ajax({
+        type: "GET",
+        url: a,
+        data: '',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: OnSuccessResult,
+        error: OnError
+    });
+    function OnSuccessResult(temperatureChart) {
+        var chartLables = temperatureChart[0];
+        var chartData = temperatureChart[1];
+        chartTwind.updateOptions({
+            xaxis: {
+                categories: chartLables
+            },
+            series: [
+                {
+                    data: chartData
+                }
+            ]
+        })
+    }
+    function OnError(err) {
+
+    }
+}
+
+//PWind chart
+renderPW = function () {
+    $.ajax({
+        type: "GET",
+        url: "/Home/GetRhData1",
+        data: '',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: OnSuccessResult,
+        error: OnError
+    });
+    function OnSuccessResult(temperatureChart) {
+        var chartLables = temperatureChart[0];
+        var chartData = temperatureChart[1];
+        var pwind = {
+            chart: {
+                height: 280,
+                type: "area"
+            },
+            dataLabels: {
+                enabled: false
+            },
+            series: [
+                {
+                    name: "p воздуха",
+                    data: chartData
+                }
+            ],
+            fill: {
+                type: "gradient",
+                gradient: {
+                    shadeIntensity: 1,
+                    opacityFrom: 0.7,
+                    opacityTo: 0.9,
+                    stops: [0, 90, 100]
+                }
+            },
+            xaxis: {
+                categories: chartLables
+            }
+        };
+
+        chartPwind = new ApexCharts(document.querySelector(".form__chart--pwind"), pwind);
+        chartPwind.render();
+    }
+    function OnError(err) {
+
+    }
+}
+
+updatePW = function () {
+    var a = "/Home/GetRhData1";
+    var tRadio = document.querySelector('input[name=PW]:checked');
+    if (tRadio.value == 1) {
+        a = "/Home/GetRhData1";
+    }
+    else if (tRadio.value == 3) {
+        a = "/Home/GetRhData3";
+    }
+    $.ajax({
+        type: "GET",
+        url: a,
+        data: '',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: OnSuccessResult,
+        error: OnError
+    });
+    function OnSuccessResult(temperatureChart) {
+        var chartLables = temperatureChart[0];
+        var chartData = temperatureChart[1];
+        chartPwind.updateOptions({
+            xaxis: {
+                categories: chartLables
+            },
+            series: [
+                {
+                    data: chartData
+                }
+            ]
+        })
+    }
+    function OnError(err) {
+
+    }
+}
+
+//TPov chart
+renderTpov = function () {
+    $.ajax({
+        type: "GET",
+        url: "/Home/GetTpovData1",
+        data: '',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: OnSuccessResult,
+        error: OnError
+    });
+    function OnSuccessResult(temperatureChart) {
+        var chartLables = temperatureChart[0];
+        var chartData = temperatureChart[1];
+        var tpov = {
+            chart: {
+                height: 280,
+                type: "area"
+            },
+            dataLabels: {
+                enabled: false
+            },
+            series: [
+                {
+                    name: "t поверхности почвы",
+                    data: chartData
+                }
+            ],
+            fill: {
+                type: "gradient",
+                gradient: {
+                    shadeIntensity: 1,
+                    opacityFrom: 0.7,
+                    opacityTo: 0.9,
+                    stops: [0, 90, 100]
+                }
+            },
+            xaxis: {
+                categories: chartLables
+            }
+        };
+
+        chartTpov = new ApexCharts(document.querySelector(".form__chart--tpov"), tpov);
+        chartTpov.render();
+    }
+    function OnError(err) {
+
+    }
+}
+updateTpov = function () {
+    var a = "/Home/GetTpovData1";
+    var tRadio = document.querySelector('input[name=TS]:checked');
+    if (tRadio.value == 1) {
+        a = "/Home/GetTpovData1";
+    }
+    else if (tRadio.value == 3) {
+        a = "/Home/GetTpovData3";
+    }
+    $.ajax({
+        type: "GET",
+        url: a,
+        data: '',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: OnSuccessResult,
+        error: OnError
+    });
+    function OnSuccessResult(temperatureChart) {
+        var chartLables = temperatureChart[0];
+        var chartData = temperatureChart[1];
+        chartTpov.updateOptions({
+            xaxis: {
+                categories: chartLables
+            },
+            series: [
+                {
+                    data: chartData
+                }
+            ]
+        })
+    }
+    function OnError(err) {
+
+    }
+}
+
+//WindSpeed chart
+renderPC = function () {
+    $.ajax({
+        type: "GET",
+        url: "/Home/GetWindData1",
+        data: '',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: OnSuccessResult,
+        error: OnError
+    });
+    function OnSuccessResult(temperatureChart) {
+        var chartLables = temperatureChart[0];
+        var chartData = temperatureChart[1];
+        var pc = {
+            chart: {
+                height: 280,
+                type: "area"
+            },
+            dataLabels: {
+                enabled: false
+            },
+            series: [
+                {
+                    name: "скорость ветра",
+                    data: chartData
+                }
+            ],
+            fill: {
+                type: "gradient",
+                gradient: {
+                    shadeIntensity: 1,
+                    opacityFrom: 0.7,
+                    opacityTo: 0.9,
+                    stops: [0, 90, 100]
+                }
+            },
+            xaxis: {
+                categories: chartLables
+            }
+        };
+
+        charWindSpeed = new ApexCharts(document.querySelector(".form__chart--precipitation"), pc);
+        charWindSpeed.render();
+    }
+    function OnError(err) {
+
+    }
+}
+
+updatePC = function () {
+    var a = "/Home/GetWindData1";
+    var tRadio = document.querySelector('input[name=PC]:checked');
+    if (tRadio.value == 1) {
+        a = "/Home/GetWindData1";
+    }
+    else if (tRadio.value == 3) {
+        a = "/Home/GetWindData3";
+    }
+    $.ajax({
+        type: "GET",
+        url: a,
+        data: '',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: OnSuccessResult,
+        error: OnError
+    });
+    function OnSuccessResult(temperatureChart) {
+        var chartLables = temperatureChart[0];
+        var chartData = temperatureChart[1];
+        charWindSpeed.updateOptions({
+            xaxis: {
+                categories: chartLables
+            },
+            series: [
+                {
+                    data: chartData
+                }
+            ]
+        })
+    }
+    function OnError(err) {
+
+    }
+}
+
+$(function () {
+    $("input[type=radio][name=TW]").change(() => updateTW());
+    $("input[type=radio][name=PW]").change(() => updatePW());
+    $("input[type=radio][name=TS]").change(() => updateTpov());
+    $("input[type=radio][name=PC]").change(() => updatePC());
+});
+
+$(document).ready(function () {
+    renderTW();
+    renderPW();
+    renderTpov();
+    renderPC();
+})
+
+var calendarButton = document.querySelector(".main-header__calendar");
+$(".main-header__calendar").on("change", function () {
+    var calendarValue = this.value;
+    var url = "/Home/GetCalendarDate";
+    $.get(url, { date: calendarValue });
+    console.log(calendarValue);
+});
+
+
+//calendarButton.addEventListener("change", function () {
+//    var calendarValue = document.querySelector(".main-header__calendar").value;
+//    var url = "/Home/GetCalendarDate";
+//    $.get(url, { date: calendarValue });
+//    console.log(calendarValue);
+//    window.location.reload();
+//});
