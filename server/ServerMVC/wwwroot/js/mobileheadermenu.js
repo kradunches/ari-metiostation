6a1888e1afee3125ac1b097443afb71de5f82fd9
kradunches <cheryaev@yandex.ru>
2023-12-01 22:12:46 +0300

    added pop-up mobile menu

---
+++ server/ServerMVC/wwwroot/js/mobileheadermenu.js
@@ -0,0 +1,14 @@
+﻿let navMain = document.querySelector('.main-header__top');
+let navToggle = document.querySelector('.main-header__top__toggle');
+
+navMain.classList.remove('main-header__top--nojs');
+
+navToggle.addEventListener('click', function () {
+    if (navMain.classList.contains('main-header__top--closed')) {
+        navMain.classList.remove('main-header__top--closed');
+        navMain.classList.add('main-header__top--opened');
+    } else {
+        navMain.classList.add('main-header__top--closed');
+        navMain.classList.remove('main-header__top--opened');
+    }
+});
